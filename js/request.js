f6b360bf68d6f5d24d034f0705d2624928c8b0e6
maohhgg <maohhgg@gmail.com>
2017-03-25 04:58:40 +0800

    重构并闭包，完成上一页的操作

--- js/request.js
+++ js/request.js
@@ -4,171 +4,173 @@
 var date, json;
 
 dateSimple = new Date();
-window.date = dateSimple.getFullYear() + '/' + (dateSimple.getMonth() + 1) + '/' +
+window.date = dateSimple.getFullYear() + '/' +
+        ((dateSimple.getMonth() + 1) < 10 ? (0+""+(dateSimple.getMonth() + 1)) : (dateSimple.getMonth() + 1)) + '/' +
         (dateSimple.getDate() < 10 ? 0+""+dateSimple.getDate() : dateSimple.getDate());
 window.status = null;
 window.error = null;
 
-function slides() {
-    var target = null;
-}
+var slides = function(node) {
+    var target = node;
+    return{
+        addVideo : function (data) {
+            var child = target.find('#' + data['date']);
 
-slide = new slides();
-slide.target = $("#slides");
+            child.append('<video class="responsive-video" id="video' + data.date + '" style="display:none" controls loop="loop">\
+                    <source src="resource/' + data.date + '_video.mp4" type="video/mp4">\
+                </video>');
 
-function videoLoop(video) {
-    if (video.ended)
-        video.play();
-}
+            var video = child.find("#video" + data.date);
+            video.ready(function() {
+                video.show();
+                child.find('img').hide();
+                video = video.get(0);
+                video.play();
+            }(video, child));
+        },
+        addChlid : function(data) {
+            data = jQuery.parseJSON(data);
+            console.log(data);
 
-function ajaxUpdate() {
-    var request = null;
-}
-ajaxUpdate.prototype.ajaxDate = function (date) {
-    this.request = $.ajax({
-       url: window.location.href + "ajax",
-       data: "main=" + date.replace(/\//g, ""),
-       statusCode: {
-           200: function() {
-               window.status = 200;
-           },
-           404: function() {
-               window.status = 404;
-           },
-           405: function() {
-               window.status = 405;
-           },
-       }
-   });
-};
+            target.append('<li id="' + data.date + '" ></li>');
 
-slides.prototype.addChlid = function(data) {
-    data = jQuery.parseJSON(data);
-    console.log(data);
+            var child = target.find('#' + data.date);
+            var img = data.date;
+            if (data.video != '0')
+                img += "_video_image";
 
-    this.target.append('<li id="' + data.date + '" ></li>');
+            child.append('<img src=" ./resource/' + img + '.jpg" >');
+            var align = ["left", "right", "center"];
+            child.append('\
+            <div class="caption ' + align[parseInt(Math.random() * 3)] + '-align" >\
+                <h3>' + data.title + '</h3>\
+                <h6 class="light grey-text text-lighten-3">' + data.content + '</h6>\
+            </div>');
 
-    var child = this.target.find('#' + data.date);
-    var img = data.date;
-    if (data.video != '0')
-        img += "_video_image";
-
-    child.append('<img src=" ./resource/' + img + '.jpg" >');
-    var align = ["left", "right", "center"];
-    child.append('\
-    <div class="caption ' + align[parseInt(Math.random() * 3)] + '-align" >\
-        <h3>' + data.title + '</h3>\
-        <h6 class="light grey-text text-lighten-3">' + data.content + '</h6>\
-    </div>');
-
-    $('.slider').slider({
-        indicators: false,
-    });
-    $('.slider').slider('pause');
-    if (data.video != '0')
-        this.addVideo(data);
-}
-slides.prototype.addVideo = function(data) {
-    var child = this.target.find('#' + data['date']);
-
-    child.append('<video class="responsive-video" id="video' + data.date + '" style="display:none" controls loop="loop">\
-            <source src="resource/' + data.date + '_video.mp4" type="video/mp4">\
-        </video>');
-
-    var video = child.find("#video" + data.date);
-    video.ready(function() {
-        video.show();
-        child.find('img').hide();
-        video = video.get(0);
-        video.play();
-    }(video, child));
-}
-
-var controls = {
-    showDate: '',
-    date: window.date,
-    slides: [],
-    previousDay: function() {
-        if (this.showDate == "") {
-            this.showDate = this.date;
-            if (this.slides.length == 0) {
-                this.slides.push(this.date);
-            }
-        }
-
-        dateSimple = new Date(new Date(this.showDate) - 24 * 60 * 60 * 1000);
-        preDate = dateSimple.getFullYear() + '/' + (dateSimple.getMonth() + 1) + '/' +
-                (dateSimple.getDate() < 10 ? 0+""+dateSimple.getDate() : dateSimple.getDate());
-        console.log(this.slides);
-        console.log(this.slides.includes(preDate.toString()));
-        if (!this.slides.includes(preDate.toString())) {
-            ajax = new ajaxUpdate();
-            ajax.ajaxDate(preDate);
-            ajax.request.done(function(data, status) {
-                if (status) {
-                    slide.addChlid(data);
-                    $('.slider').slider('start');
-                    $('.slider').slider('next');
-                    $('.slider').slider('pause');
-                }
+            $('.slider').slider({
+                indicators: false,
             });
-            this.slides.push(preDate);
+            $('.slider').slider('pause');
+            if (data.video != '0')
+                this.addVideo(data);
         }
-        console.log(ajaxUpdate);
-        this.showDate = preDate;
-
-    },
-    nextDay: function() {
-        if (this.showDate == "") {
-            this.showDate = this.date;
-            if (this.slides.length == 0) {
-                this.slides.push(this.date);
-            }
-        }
-        console.log(this);
-
-        dateSimple = new Date(new Date(this.showDate) + 25 * 60 * 60 * 1000);
-        console.log(dateSimple);
-        if (dateSimple > new Date(new Date(this.date) + 48 * 60 * 60 * 1000)) {
-            Materialize.toast("超出日期范围", 4000);
-            return;
-        }
-        nextDate = dateSimple.getFullYear() + '/' + (dateSimple.getMonth() + 1) + '/' +
-                (dateSimple.getDate() < 10 ? 0+""+dateSimple.getDate() : dateSimple.getDate());
-
-        console.log(this.slides);
-        console.log(this.slides.includes(nextDate.toString()));
-        if (!this.slides.includes(nextDate)) {
-            ajax = new ajaxUpdate();
-            ajax.ajaxDate(nextDate);
-            ajax.request.done(function(data, status) {
-                if (status) {
-                    slide.addChlid(data);
-                    $('.slider').slider('start');
-                    $('.slider').slider('prev');
-                    $('.slider').slider('pause');
-                }
-            });
-            this.slides.push(nextDate);
-        }
-        Materialize.toast(nextDate + "执行了", 4000);
-        this.showDate = nextDate;
-    },
-    chooseData: function() {
-
-    },
-    downloadImages: function() {
-
     }
 }
 
-ajax = new ajaxUpdate();
-ajax.ajaxDate(window.date);
+slide = new slides($("#slides"));
+
+var Ajax = function (date) {
+    this.request = $.ajax({
+           url: window.location.href + "ajax",
+           data: "main=" + date.replace(/\//g, ""),
+           statusCode: {
+               200: function() {
+                   window.status = 200;
+               },
+               404: function() {
+                   window.status = 404;
+               },
+               405: function() {
+                   window.status = 405;
+               },
+           }
+       });
+}
+
+
+var Controls = function(num){
+    var showDate ='';
+    var date = num;
+    var slides = [];
+    return {
+        previousDay: function() {
+            if (showDate == "") {
+                showDate = date;
+                if (slides.length == 0) {
+                    slides.push(this.date);
+                }
+            }
+
+            var dateSimple = new Date(new Date(showDate) - 24 * 60 * 60 * 1000);
+            var preDate = dateSimple.getFullYear() + '/' +
+                    ((dateSimple.getMonth() + 1) < 10 ? (0+""+(dateSimple.getMonth() + 1)) : (dateSimple.getMonth() + 1)) + '/' +
+                    (dateSimple.getDate() < 10 ? 0+""+dateSimple.getDate() : dateSimple.getDate());
+            console.log(preDate);
+            console.log(slides.includes(preDate.toString()));
+            if (!slides.includes(preDate.toString())) {
+                var ajax = new Ajax(preDate.toString());
+                ajax.request.done(function(data, status) {
+                    if (status) {
+                        slide.addChlid(data);
+                        $('.slider').slider('start');
+                        $('.slider').slider('next');
+                        $('.slider').slider('pause');
+                    }
+                });
+                slides.push(preDate);
+            } else {
+                $('.slider').slider('start');
+                $('.slider').slider('next');
+                $('.slider').slider('pause');
+            }
+            showDate = preDate;
+        },
+        nextDay: function() {
+            if (showDate == "") {
+                showDate = date;
+                if (slides.length == 0) {
+                    slides.push(date);
+                }
+            }
+
+            var dateSimple = new Date(new Date(showDate) + 48 * 60 * 60 * 1000);
+            console.log(dateSimple);
+            if (dateSimple > new Date(new Date(date) + 48 * 60 * 60 * 1000)) {
+                Materialize.toast("超出日期范围", 4000);
+                return;
+            }
+            var nextDate = dateSimple.getFullYear() + '/' +
+                    ((dateSimple.getMonth() + 1) < 10 ? (0+""+(dateSimple.getMonth() + 1)) : (dateSimple.getMonth() + 1)) + '/' +
+                    (dateSimple.getDate() < 10 ? 0+""+dateSimple.getDate() : dateSimple.getDate());
+            console.log(nextDate);
+            console.log(nextDate.toString());
+            console.log(slides.includes(nextDate.toString()));
+            if (!slides.includes(nextDate.toString())) {
+                var ajax = new Ajax(nextDate.toString());
+                ajax.request.done(function(data, status) {
+                    if (status) {
+                        slide.addChlid(data);
+                        $('.slider').slider('start');
+                        $('.slider').slider('prev');
+                        $('.slider').slider('pause');
+                    }
+                });
+                slides.push(nextDate);
+            } else {
+                $('.slider').slider('start');
+                $('.slider').slider('prev');
+                $('.slider').slider('pause');
+            }
+            Materialize.toast(nextDate + "执行了", 4000);
+            this.showDate = nextDate;
+        },
+        chooseData: function() {
+
+        },
+        downloadImages: function() {
+
+        }
+    }
+
+}
+var ajax = new Ajax(window.date);
+console.log(ajax);
 ajax.request.done(function(data,status){
     if (status) {
         slide.addChlid(data);
     }
 })
+var controls = new Controls(window.date)
 $('.previousDay').get(0).addEventListener('click', function() {
     controls.previousDay();
     console.log(controls);
